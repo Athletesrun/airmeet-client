d67083f39da617e9fb973267a3de1da52d362f8c
Ben Wingerter <benwingerter01@gmail.com>
2017-02-25 18:36:31 -0600

    Conversations page start

--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -3,30 +3,29 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-page2',
-  templateUrl: 'page2.html'
+  templateUrl: "conversations.html"
 })
+
+export class Conversation {
+    item;
+    constructor(params: NavParams) {
+        this.item = params.data.item;
+    }
+}
+
+@Component({selector: 'page-page2', templateUrl: 'page2.html'})
+
 export class Page2 {
   selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{name: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.items = [
+        {name: "Carl"}
+    ];
   }
 
   itemTapped(event, item) {
