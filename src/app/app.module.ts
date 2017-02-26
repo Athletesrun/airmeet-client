d56b33a3b8774a35ceba254493dd5e046d31378a
Daniel Noon <supercookies@outlook.com>
2017-02-25 18:14:15 -0600

    Hello, word!

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1, Person } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import {You} from '../pages/page3/page3';
+import {Map} from '../pages/page4/page4';
+import {Settings} from '../pages/page5/page5';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Page1,
+    Page2,
+    Person,
+    You,
+    Map,
+    Settings
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Page1,
+    Page2,
+    Person,
+    You,
+    Map,
+    Settings
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
