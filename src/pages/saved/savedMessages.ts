a1f53fb426c587545285cf49ea35e160299db775
Daniel Noon <supercookies@outlook.com>
2017-04-04 00:06:06 -0500

    Saved messages complete!

--- src/pages/saved/savedMessages.ts
+++ src/pages/saved/savedMessages.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+import {HttpService} from "../../services/http.service";
+import {Conversation} from "../messages/messages"
 
 @Component({
     selector: 'saved-messaged',
-    templateUrl: 'savedMessages.html'
+    templateUrl: 'savedMessages.html',
+    providers: [HttpService]
 })
 
 export class SavedMessages {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    conversations;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, private api: HttpService) {
 
     }
 
-    ngOnInit() {
+    conversationTapped(user) {
+      this.navCtrl.push(Conversation, {
+        person: user
+      })
+    }
 
+    ngOnInit() {
+      this.api.getSavedConversations().subscribe((res) => {
+        console.log(res)
+        this.conversations = res;
+      })
     }
 
 }
